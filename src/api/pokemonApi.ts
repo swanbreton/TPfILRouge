aa085cb8d8faf8c083eda72c8a0789bfcedcc162
swanbreton <swan.breton75@gmail.com>
2025-06-20 12:34:39 +0200

    final

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -1,21 +1,19 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+  import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const pokemonApi = createApi({
-  reducerPath: "pokemonApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
-  endpoints: (builder) => ({
-    // Récupérer un Pokémon par ID
-    getPokemonById: builder.query<any, number>({
-      query: (id) => `pokemon/${id}`,
+  export const pokemonApi = createApi({
+    reducerPath: "pokemonApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+    endpoints: (builder) => ({
+      getPokemonById: builder.query<any, number>({
+        query: (id) => `pokemon/${id}`,
+      }),
+      getPokemonByGeneration: builder.query<any, number>({
+        query: (gen) => `generation/${gen}`,
+      }),
     }),
-    // Récupérer tous les Pokémon d'une génération
-    getPokemonByGeneration: builder.query<any, number>({
-      query: (gen) => `generation/${gen}`,
-    }),
-  }),
-});
+  });
 
-export const { 
-  useGetPokemonByIdQuery,  
-  useGetPokemonByGenerationQuery
-} = pokemonApi;
+  export const { 
+    useGetPokemonByIdQuery,  
+    useGetPokemonByGenerationQuery
+  } = pokemonApi;
